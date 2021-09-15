155514b3b4bea961877001179f9f12fa30d784bc
franciellebrozoski <86450198+franciellebrozoski@users.noreply.github.com>
2021-09-15 17:17:50 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+var nome = "Francielle"
+var NotaDoPrimeiroBimestre = 9
+var NotaDoSegundoBimestre = 9
+var NotaDoTerceiroBimestre = 8
+var NotaDoQuartoBimestre = 8
+var NotaFinal = (NotaDoPrimeiroBimestre + NotaDoSegundoBimestre + NotaDoTerceiroBimestre + NotaDoQuartoBimestre)/ 4
+
+var notafixada = NotaFinal.toFixed(1)
+
+console.log("Hello World! " + nome)
+console.log(NotaDoPrimeiroBimestre)
+console.log(NotaFinal)
+
